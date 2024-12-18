5f1d4a719e8c9c36ceeea7d2c9e61789e606e7c5
minhazapon <minhajulabedinapon@gmail.com>
2024-12-17 21:34:26 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,29 @@
+import { NavLink } from "react-router-dom";
 
 
 
 const Nav = () => {
+
+    
+    const nav = <>
+    
+       <li className=" droFont " ><NavLink to='/'>Home</NavLink></li>
+       <li className=" droFont " ><NavLink to='/shop'>Shop</NavLink></li>
+       <li className=" droFont " ><NavLink to='/product'>Products</NavLink></li>
+       <li className=" droFont " ><NavLink to='/package'>Package</NavLink></li>
+       <li className=" droFont " ><NavLink to='/about'>About</NavLink></li>
+       <li className=" droFont " ><NavLink to='/service'>Service</NavLink></li>
+    
+    
+    </>
+
+
+
     return (
         <div>
 
 
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 shadow p-5 ">
               <div className="navbar-start">
                 <div className="dropdown">
                   <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -26,36 +43,18 @@ const Nav = () => {
                   <ul
                     tabIndex={0}
                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><a>Item 1</a></li>
-                    <li>
-                      <a>Parent</a>
-                      <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                      </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                  {nav}
                   </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <div className=" flex items-center gap-2">
+                 <img className=" h-[40px] w-[40px] " src="https://cdn-icons-gif.flaticon.com/15568/15568296.gif" alt="" />
+                 <a className=" text-2xl droFont">ApoDrone</a>
+                </div>
               </div>
               <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                  <li><a>Item 1</a></li>
-                  <li>
-                    <details>
-                      <summary>Parent</summary>
-                      <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                      </ul>
-                    </details>
-                  </li>
-                  <li><a>Item 3</a></li>
+                 {nav}
                 </ul>
-              </div>
-              <div className="navbar-end">
-                <a className="btn">Button</a>
               </div>
             </div>
             
