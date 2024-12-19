ba8ba6aef06f885e342b79425a8707251fd68c6e
minhazapon <minhajulabedinapon@gmail.com>
2024-12-19 01:41:39 -0800

    added file

--- src/home file/Drone.jsx
+++ src/home file/Drone.jsx
@@ -1,19 +1,43 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import DroneDetails from "./DroneDetails";
 
 
 
 const Drone = () => {
 
+
+    const [ drone, setDrone ] = useState([])
+
+
+    useEffect( () =>{
+    fetch('https://raw.githubusercontent.com/minhazapon/ApoDro--react.js-project/refs/heads/main/public/drone.json')
+    .then( res => res.json())
+    .then(data => setDrone(data))
+    } , [])
+
     return (
         <div className=" mt-20 mb-10 ml-10 mr-10 ">
 
-            <div>
+        <div>
+        <div>
+        <p className=" text-center text-gray-500 droFont text-xl ">Featured products</p>
+        <p className=" text-center text-black mt-5 droFont text-4xl ">DRONE PRODUCTS</p>
+        </div>
 
-            <div>
-                <p className=" text-center text-gray-500 droFont text-xl ">Featured products</p>
-                <p className=" text-center text-black mt-5 droFont text-4xl ">DRONE PRODUCTS</p>
+
+            <div className=" flex justify-center mt-10 ">
+             <div className=" grid  md:grid-cols-4 gap-10 ">
+
+                  {
+
+                      drone.map( drone => <DroneDetails drone={drone} ></DroneDetails> )
+                  
+                  }
+                    
+
+             </div>
             </div>
-
-
 
 
             </div>
--- src/home file/DroneDetails.jsx
+++ src/home file/DroneDetails.jsx
@@ -1,11 +1,32 @@
 
 
 
-const DroneDetails = () => {
+const DroneDetails = ({drone}) => {
+
+    const {image, name, price} = drone
+
     return (
+
         <div>
+
+        <div className="card bg-base-100  w-[300px] shadow-xl hover:border-[1px] ">
+        <figure className=" p-5">
+        <img src={image} alt="Shoes" className="rounded-xl h-[200px] " />
+        </figure>
+        <div className="card-body items-center text-center">
+        <h2 className="card-title text-xs droFont "> {name} </h2>
+        <div className=" flex items-center gap-2 ">
+         <p className=" droFont text-cyan-400 font-semibold ">{price}</p>
+         <p className=" droFont line-through text-gray-400 ">$5000</p>
+        </div>
+        <div className="card-actions">
+        <button className="btn droFont border-[1px] border-gray-300 text-gray-500 ">Add Cart</button>
+        </div>
+        </div>
+        </div>
             
         </div>
+
     );
 };
 
