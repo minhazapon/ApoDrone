7b84339a5c7c79ed2843382272b724f35c93db24
minhazapon <minhajulabedinapon@gmail.com>
2024-12-19 20:19:02 -0800

    added shop items file

--- src/shop file/ShopItems.jsx
+++ src/shop file/ShopItems.jsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
 
 
 
 
 const ShopItems = () => {
     return (
-        <div className=" mt-20 mb-20 ml-10 mr-10 ">
+        <div className=" mt-20 mb-20 ml-10 mr-10 flex justify-center ">
 
 
-            <div>
+            <div className=" grid  md:grid-cols-3 gap-10">
 
                      
-
+            
+            <Link to=''>
             <div className="card bg-base-100 image-full w-96 shadow-xl">
             <figure>
             <img
@@ -18,22 +20,284 @@ const ShopItems = () => {
             alt="Shoes" />
             </figure>
             <div className="card-body">
-            <h2 className="card-title droFont ">BagPacks</h2>
+            <h2 className="card-title droFont ">BackPacks</h2>
             <p className=" droFont ">8 Items in Stock</p>
             <div className="card-actions justify-end">
              <button className="btn  bg-white droFont ">Go Shop</button>
             </div>
             </div>
             </div>
+            </Link>
 
 
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/6_c62de37e-1ca9-4692-a0cc-1aac055117ea.jpg?crop=center&height=600&v=1573705093&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Bag</h2>
+            <p className=" droFont ">6 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/9_5b62c585-22a1-4554-9637-ebc0f1dadd37.jpg?crop=center&height=600&v=1573700052&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Cycle</h2>
+            <p className=" droFont ">8 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
 
 
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/collections/coffee_coll.png?crop=center&height=600&v=1574217718&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Coffe Maker</h2>
+            <p className=" droFont ">6 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
 
 
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/7_a5088e25-e3d3-4066-b33d-47cad2cf768a.jpg?crop=center&height=600&v=1573665128&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">BagPacks</h2>
+            <p className=" droFont ">6 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
 
 
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/collections/coll_drone.jpg?crop=center&height=600&v=1574218061&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Drink</h2>
+            <p className=" droFont ">6 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
 
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/5_c594e12e-20fe-4e18-b5a0-c13ae579ba2d.jpg?crop=center&height=600&v=1573742129&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">HeadPhone</h2>
+            <p className=" droFont ">9 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/1_f155b960-5aa4-4f6c-9797-bcd2e8d274d6.jpg?crop=center&height=600&v=1573743333&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Jewelery</h2>
+            <p className=" droFont ">8 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/5-800x800_c58c9742-4b0e-47e2-bbe1-ef4dfb911d57.jpg?crop=center&height=600&v=1534613220&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Scooty</h2>
+            <p className=" droFont ">6 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/collections/coll_shaver.jpg?crop=center&height=600&v=1574218641&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Shaver</h2>
+            <p className=" droFont ">6 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/2_bd2f834c-dd10-4f44-91a8-64fa8b6f4af2.jpg?crop=center&height=600&v=1573744246&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Shoes</h2>
+            <p className=" droFont ">9 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/5-800x980_ee8a0e22-cbbd-4a15-bf43-be31cf085f8e.jpg?crop=center&height=600&v=1574210095&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Skincare</h2>
+            <p className=" droFont ">7 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/10-800x800_e9b94a0f-5a2f-4145-a07e-fd45249f0d5e.jpg?crop=center&height=600&v=1534504052&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">SmartWatch</h2>
+            <p className=" droFont ">8 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/1_59fef813-05a2-4fe2-b10f-8ae9270ebfe2.jpg?crop=center&height=600&v=1573737772&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Staff Picks</h2>
+            <p className=" droFont ">8 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/collections/coll_stroller.jpg?crop=center&height=600&v=1574218500&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Stroller</h2>
+            <p className=" droFont ">6 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+
+            <Link to=''>
+            <div className="card bg-base-100 image-full w-96 shadow-xl">
+            <figure>
+            <img
+            src="https://elomus-theme.myshopify.com/cdn/shop/products/7_5b6c82e1-daab-445d-a891-5759d3bae9bc.jpg?crop=center&height=600&v=1573664461&width=600"
+            alt="Shoes" />
+            </figure>
+            <div className="card-body">
+            <h2 className="card-title droFont ">Wallet</h2>
+            <p className=" droFont ">5 Items in Stock</p>
+            <div className="card-actions justify-end">
+             <button className="btn  bg-white droFont ">Go Shop</button>
+            </div>
+            </div>
+            </div>
+            </Link>
+
+
+        
 
             </div>
             
